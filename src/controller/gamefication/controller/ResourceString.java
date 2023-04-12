69f073ab9247457dd69f34409e49375be9d759e4
iagoholekdev <augustinhocarrara43@gmail.com>
2023-04-11 22:56:49 -0300

    Criação de resource

--- src/controller/gamefication/controller/ResourceString.java
+++
@@ -1,7 +0,0 @@
-package controller.gamefication.controller;
-
-public class ResourceString {
-    final String Welcome = "Sejam bem-vindos ao ChatBot";
-    final String ChooseDificuldade = "Escolha a dificuldade";
-
-}
---
+++ src/resources/ResourceString.java
@@ -0,0 +1,8 @@
+package resources;
+
+public class ResourceString {
+   public final int primeiraPergunta = 1;
+    public final int segundaPergunta = 2;
+    public final int terceiraPergunta = 3;
+
+}
